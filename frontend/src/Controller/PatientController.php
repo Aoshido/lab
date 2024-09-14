85f3fb772621050d546ceb4b4a5d16ce68f10bc9
aoshido <aoshido@gmail.com>
2024-09-14 19:04:07 -0300

    Add view to create sera

--- frontend/src/Controller/PatientController.php
+++ frontend/src/Controller/PatientController.php
@@ -5,6 +5,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -18,8 +19,8 @@ class PatientController extends AbstractController
         $this->httpClient = $httpClient;
     }
 
-    #[Route('/patients', name: 'app_patients')]
-    public function index(): Response
+    #[Route('/patients', name: 'patients_list')]
+    public function patientsList(): Response
     {
         $response = $this->httpClient->request('GET', 'http://php/api/api/patients');
         $patients = $response->toArray()['hydra:member'];
@@ -27,6 +28,49 @@ class PatientController extends AbstractController
         return $this->render('patient/index.html.twig', [
             'patients' => $patients,
         ]);
+    }
+
+
+    #[Route('/serum', name: 'serum_create')]
+    public function serumCreate(Request $request): Response
+    {
+        // Check if the form was submitted
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            $dateCheckbox = $request->request->get('custom_date');
+
+            if ($dateCheckbox) {
+                $dateString = $request->request->get('date');
+                $dateTime = new \DateTime($dateString);
+                $date = $dateTime->format('Y-m-d');
+            } else {
+                // Use the current date if no custom date is provided
+                $date = (new \DateTime())->format('Y-m-d');
+            }
+
+            $payload = [
+                "name" => $name,
+                "serums" => [
+                    [
+                        "extractionDate" => $date
+                    ]
+                ]
+            ];
+
+            // Post data to /api/patients
+            $response = $this->httpClient->request('POST', 'http://php/api/api/patients', [
+                'json' => $payload
+            ]);
+
+            if ($response->getStatusCode() === 201) {
+                return $this->redirectToRoute('patients_list');
+            } else {
+                dump($response);die();
+                return new Response('Error: Unable to create patient', 500);
+            }
+        }
+
+        return $this->render('patient/serum_create.html.twig');
     }
 }
 
