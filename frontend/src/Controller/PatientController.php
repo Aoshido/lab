47d72c285eaed80c16e9e448cb0776f666aa2879
aoshido <aoshido@gmail.com>
2024-12-15 12:47:18 -0300

    Move base api URL to parameter

--- frontend/src/Controller/PatientController.php
+++ frontend/src/Controller/PatientController.php
@@ -9,14 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 
 class PatientController extends AbstractController
 {
     private $httpClient;
+    private $baseApiUrl;
 
-    public function __construct(HttpClientInterface $httpClient)
+    public function __construct(HttpClientInterface $httpClient,string $baseApiUrl)
     {
         $this->httpClient = $httpClient;
+        $this->baseApiUrl = $baseApiUrl;
     }
 
     #[Route('/', name: 'patients_list_default')]
@@ -27,7 +31,7 @@ class PatientController extends AbstractController
         $searchDni = $request->query->get('search_dni');
         $page = $request->query->getInt('page', 1);
 
-        $url = 'http://php/api/api/patients';
+        $url = $this->baseApiUrl . 'patients';
         $queryParams = [];
 
         if ($searchName) {
@@ -111,7 +115,7 @@ class PatientController extends AbstractController
             ];
 
             // Post data to /api/patients
-            $response = $this->httpClient->request('POST', 'http://php/api/api/patients', [
+            $response = $this->httpClient->request('POST', $this->baseApiUrl . 'patients', [
                 'json' => $payload
             ]);
 
